7e977268ddd83a19479015b3195aa49985d9c2de
Vadym Holovin <holyvad@gmail.com>
2019-11-26 21:42:20 +0200

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+
+/**
+ * Accordion
+ */
+$(document).ready(function () {
+  let button = $('.js-accordion button');
+
+  button.click(function (e) { 
+    e.preventDefault();
+
+    if (!$(this).hasClass('is-active')) {
+      button.removeClass('is-active').attr('aria-expanded', false);
+      button.parent().next().removeClass('is-open');
+      $(this).addClass('is-active').attr('aria-expanded', true);
+      $(this).parent().next().addClass('is-open');
+    } else {
+      $(this).removeClass('is-active').attr('aria-expanded', false);
+      $(this).parent().next().removeClass('is-open');
+    }
+  });
+});
