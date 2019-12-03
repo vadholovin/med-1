0b17b681f5060b32bf0d52abb89926d874ec5891
Vadym Holovin <holyvad@gmail.com>
2019-12-03 18:27:46 +0200

    Changed markup for modals

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,5 @@
 
 /**
- * Accordion
- */
-$(document).ready(function () {
-  let button = $('.js-accordion button');
-
-  button.click(function (e) { 
-    e.preventDefault();
-
-    if (!$(this).hasClass('is-active')) {
-      button.removeClass('is-active').attr('aria-expanded', false);
-      button.parent().next().removeClass('is-open');
-      $(this).addClass('is-active').attr('aria-expanded', true);
-      $(this).parent().next().addClass('is-open');
-    } else {
-      $(this).removeClass('is-active').attr('aria-expanded', false);
-      $(this).parent().next().removeClass('is-open');
-    }
-  });
-});
-
-
-/**
  * Modals
  */
 $(document).ready(function () {
@@ -33,5 +11,13 @@ $(document).ready(function () {
     });
     return false;
   });
+
+  // $('.modal').on($.modal.OPEN, function () {
+  //   $('html').css('overflow', 'hidden');
+  // });
+
+  // $('.modal').on($.modal.AFTER_CLOSE, function () {
+  //   $('html').removeAttr('style');
+  // });
   
 });
